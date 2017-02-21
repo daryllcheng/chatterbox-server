48eeba6c97a78c10b0d737d598f1b5b5611a9b38
MKS-Student <mks.student@hackreactor.com>
2017-02-20 16:35:39 -0800

    Initial setup

--- server/request-handler.js
+++ server/request-handler.js
@@ -12,7 +12,10 @@ this file and include it in basic-server.js so that it actually works.
 
 **************************************************************/
 
-var requestHandler = function(request, response) {
+// var exports = module.exports = {};
+// var url = require('url');
+
+module.exports.requestHandler = function(request, response) {
   // Request and Response come from node's http module.
   //
   // They include information about both the incoming request, such as
@@ -40,10 +43,11 @@ var requestHandler = function(request, response) {
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
   headers['Content-Type'] = 'text/plain';
+  // headers['content-type'] = 'application/json';
 
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
+  // response.writeHead(statusCode, headers);
 
   // Make sure to always call response.end() - Node may not send
   // anything back to the client until you do. The string you pass to
@@ -52,7 +56,17 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end('Hello, World!');
+  // response.end('Hello, World!');
+
+  if (request.method === 'GET' && request.url === '/classes/messages') {
+    response.writeHead(statusCode, headers);
+    response.end('Hello, World!');
+  }
+
+  if (request.method === 'POST' && request.url === '/classes/messages') {
+    response.writeHead(statusCode, headers);
+    response.end('Hello, World!'); 
+  }  
 };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
